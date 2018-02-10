dac2cb8606c882ea56694a432812d70dd5121236
Yoshino <weihaomouse@gmail.com>
2018-02-09 21:29:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
-from Slack_API import Slack
+from slackclient import SlackClient
 from slackeventsapi import SlackEventAdapter
 from flask import Flask
 
+app = Flask(__name__)
+
+# found in OAuth and Permission section
 SLACK_BOT_TOKEN = ''
+
+# Part of App Credentials
 SLACK_VERIFICATION_TOKEN = ''
-bot = Slack(SLACK_BOT_TOKEN, SLACK_VERIFICATION_TOKEN)
-slack_events_adapter = SlackEventAdapter(bot.verf_token, endpoint="/slack/events", server=app)
+
+slack_client = SlackClient(SLACK_BOT_TOKEN)
+slack_events_adapter = SlackEventAdapter(SLACK_VERIFICATION_TOKEN, endpoint="/slack/events", server=app)
+
 
 @slack_events_adapter.on("message")
-# requires 'message' scope (Event API)
+# requires 'message' scope
 def handle_message(event_data):
     message = event_data["event"]
     if message.get("subtype") is None:
         channel = message["channel"]
-        message = "<@%s>! You typed: " % message["user"] + message["text"] + " :simple_smile:"
-        bot.send_message(channel=channel, text=message)
+        message = "<@%s> just typed: " % message["user"] + message["text"] + " :simple_smile:"
+        slack_client.api_call("chat.postMessage", channel=channel, text=message)
         
 @slack_events_adapter.on("reaction_added")
-# requires 'reaction_added' scope (Event API)
+# requires 'reaction_added' scope
 def reaction_added(event_data):
     event = event_data["event"]
     emoji = event["reaction"]
     channel = event["item"]["channel"]
     text = ":%s:" % emoji
-    bot.send_message(channel=channel, text=message)
+    slack_client.api_call("chat.postMessage", channel=channel, text=text)
