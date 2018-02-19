fb8ddcdd35a058787d7076acdcd6234e3d863469
Corey Noel <corey.g.noel@gmail.com>
2018-02-19 14:29:55 -0500

    updating app to use env vars, updating send message script to support json

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from slackclient import SlackClient
 from slackeventsapi import SlackEventAdapter
-from flask import Flask
-import click
+from flask import Flask, request
+import click, os
 
-app = Flask(__name__)
-app.config.from_json("credentials.json")
-slack_client = SlackClient(app.config["SLACK_BOT_TOKEN"])
-slack_events_adapter = SlackEventAdapter(app.config["SLACK_VERIFICATION_TOKEN"], endpoint="/slack/events", server=app)
 
-@slack_events_adapter.on("message")
+def main():
+    app = Flask(__name__)
+
+    success = True
+
+    success = success and import_env_var(app, "SLACK_BOT_TOKEN")
+    success = success and import_env_var(app, "SLACK_VERIFICATION_TOKEN")
+
+    if not success:
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+
+    slack_client = SlackClient(app.config["SLACK_BOT_TOKEN"])
+    slack_events_adapter = SlackEventAdapter(app.config["SLACK_VERIFICATION_TOKEN"], endpoint="/slack/events", server=app)
+
 # requires 'message' scope
+@slack_events_adapter.on("message")
 def handle_message(event_data):
     click.echo(event_data)
     message = event_data["event"]
@@ -19,11 +32,22 @@ def handle_message(event_data):
         click.echo('Attempting to post:', msg)
         slack_client.api_call("chat.postMessage", channel=channel, text=msg)
 
-@slack_events_adapter.on("reaction_added")
 # requires 'reaction_added' scope
+@slack_events_adapter.on("reaction_added")
 def reaction_added(event_data):
     event = event_data["event"]
     emoji = event["reaction"]
     channel = event["item"]["channel"]
     text = ":%s:" % emoji
     slack_client.api_call("chat.postMessage", channel=channel, text=text)
+
+# import an environment variable as an app config option
+def import_env_var(app, env_var):
+    try:
+        app.config[env_var] = os.environ[env_var]
+        return True
+    except KeyError:
+        return False
+
+if __name__ == "__main__":
+    main()
--- slack_send_messages.py
+++ slack_send_messages.py
@@ -5,14 +5,15 @@
 # This shows an example of sending a message to Slack
 
 from slackclient import SlackClient
+import json
 
-SLACK_BOT_TOKEN = 'xoxb-311305854327-QXJDo079SwxZUFhYMsFWmbmq'
+SLACK_BOT_TOKEN = 'xoxb-313903713426-UJKPHKX598yn8PbrLV4i9Yue'
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
-msg = 'PUT YOUR MESSAGE HERE'
+msg = json.load(open("message.json"))
 
 # This will post the message to the general channel of our Yelp group
 # under the name of the bot
 # you can change the channel parameter to any other existing channel names,
 # for example '#random'
-slack_client.api_call("chat.postMessage", channel="#general", text=msg)
+slack_client.api_call("chat.postMessage", channel="#bot-practice", **msg)
