89989a1b22c71849b37db4fc0c927080392a343a
Corey Noel <corey.g.noel@gmail.com>
2018-02-20 07:12:19 -0500

    testing imoprting env vars

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
 from slackclient import SlackClient
 from slackeventsapi import SlackEventAdapter
 from flask import Flask, request
-import click, os
+import click, os, sys
 
 
 # import an environment variable as an app config option
+# throws KeyError
 def import_env_var(app, env_var):
-    try:
-        app.config[env_var] = os.environ[env_var]
-        return True
-    except KeyError:
-        return False
+    app.config[env_var] = os.environ[env_var]
 
 app = Flask(__name__)
 
-success = True
-
-success = success and import_env_var(app, "SLACK_BOT_TOKEN")
-success = success and import_env_var(app, "SLACK_VERIFICATION_TOKEN")
-
-if not success:
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+try:
+    import_env_var(app, "SLACK_BOT_TOKEN")
+    import_env_var(app, "SLACK_VERIFICATION_TOKEN")
+except KeyError:
+    click.echo("Could not load environment variables")
+    sys.exit()
 
 slack_client = SlackClient(app.config["SLACK_BOT_TOKEN"])
 slack_events_adapter = SlackEventAdapter(app.config["SLACK_VERIFICATION_TOKEN"], endpoint="/slack/events", server=app)
@@ -31,12 +24,13 @@ slack_events_adapter = SlackEventAdapter(app.config["SLACK_VERIFICATION_TOKEN"],
 # requires 'message' scope
 @slack_events_adapter.on("message")
 def handle_message(event_data):
-    print(event_data)
+    click.echo("Received a message")
+    click.echo(event_data)
     message = event_data["event"]
-    if message.get("subtype") is None and "hello world" in message.get('text'):
+    if message.get("subtype") is None and "hello" in message.get('text'):
         channel = message["channel"]
-        msg = "<@%s> just typed: " % message["user"] + message["text"] + " :simple_smile:"
-        print('Attempting to post:', msg)
+        msg = "Hi <@%s>! :simple_smile:" % message["user"]
+        click.echo('Attempting to post:', msg)
         slack_client.api_call("chat.postMessage", channel=channel, text=msg)
 
 # requires 'reaction_added' scope
@@ -47,6 +41,3 @@ def reaction_added(event_data):
     channel = event["item"]["channel"]
     text = ":%s:" % emoji
     slack_client.api_call("chat.postMessage", channel=channel, text=text)
-
-    
-
--- slack_send_messages.py
+++ slack_send_messages.py
@@ -6,6 +6,7 @@
 
 from slackclient import SlackClient
 import json
+import datetime
 
 SLACK_BOT_TOKEN = 'xoxb-313903713426-UJKPHKX598yn8PbrLV4i9Yue'
 
@@ -16,4 +17,26 @@ msg = json.load(open("message.json"))
 # under the name of the bot
 # you can change the channel parameter to any other existing channel names,
 # for example '#random'
-slack_client.api_call("chat.postMessage", channel="#bot-practice", **msg)
+send_message(channel, message)
+
+def send_message(msg, channel):
+	slack_client.api_call("chat.postMessage", channel=channel, **msg)
+
+def get_restaurant(id):
+
+	res = {} # get json from yelp
+	
+	name = res["name"]
+	link = res["url"]
+	price = res["price"]
+	image = res["image_url"]
+	rating = "\u2605" * int(res["rating"]) + "\u2606" * (5 - int(res["rating"]))
+
+	weekday = datetime.datetime.today().weekday()
+	endtime = res["hours"]["open"][weekday]["end"]
+	time = (int(endtime[:2]) % 12) + ":" + endtime[2:]
+
+	desc = ""
+	review = ""
+	address = ""
+	nav_link = ""
