d8c29e76c7f6a33e49e2732e65d8d0b284bf1239
Corey Noel <corey.g.noel@gmail.com>
2018-02-26 23:18:35 -0500

    merged with main part 2

--- app.py
+++ app.py
@@ -1,44 +1,45 @@
+import click, os, sys
 from slackclient import SlackClient
 from slackeventsapi import SlackEventAdapter
 from flask import Flask, make_response, Response, request
-import click, os, sys
+from yelp import YelpAPI
 
+
+##### SETUP
+app = Flask(__name__)
 
 # import an environment variable as an app config option
 # throws KeyError
 def import_env_var(app, env_var):
   app.config[env_var] = os.environ[env_var]
 
-app = Flask(__name__)
-
 try:
   import_env_var(app, "SLACK_BOT_TOKEN")
   import_env_var(app, "SLACK_VERIFICATION_TOKEN")
+  import_env_var(app, "YELP_API_KEY")
 except KeyError:
   click.echo("Could not load environment variables")
   sys.exit()
 
 slack_client = SlackClient(app.config["SLACK_BOT_TOKEN"])
 slack_events_adapter = SlackEventAdapter(app.config["SLACK_VERIFICATION_TOKEN"], endpoint="/slack/events", server=app)
+yelp_api = YelpAPI(app.config["YELP_API_KEY"])
+
+
+##### EVENT HANDLERS
 
 # handles button press events
 @app.route("/slack/message_actions", methods=["POST"])
 def message_actions():
-    # Parse the request payload
-    form_json = json.loads(request.form["payload"])
+  # Parse the request payload
+  form_json = json.loads(request.form["payload"])
 
-    # Check to see what the user's selection was and update the message
-    selection = form_json["actions"][0]["value"]
+  # Check to see what the user's selection was and update the message
+  selection = form_json["actions"][0]["value"]
+  message_text = "<@{0}> selected {1}".format(form_json["user"]["id"], selection)
 
-    message_text = '<@{0}> selected {1}'.format(form_json["user"]["id"], selection)
-    
-    slack_client.api_call(
-      "chat.postMessage",
-      channel=form_json["channel"]["id"],
-      text=message_text
-    )
-
-    return make_response("", 200)
+  send_message(channel=form_json["channel"]["id"], text=message_text)
+  return make_response("", 200)
 
 # requires 'message' scope
 @slack_events_adapter.on("message")
@@ -48,8 +49,16 @@ def handle_message(event_data):
     channel = message["channel"]
     user = message["user"]
 
-    if "hello" in message.get('text'):
+    if "hello" in message.get("text"):
       send_message(channel, text="Hi <@%s>! :simple_smile:" % user)
+
+    if "botsearch" in message.get("text"):
+      search(channel)
+
+
+  return make_response("", 200)
+
+##### HELPERS
 
 # send a message to channel
 # uses keyword args to expand message
@@ -57,3 +66,15 @@ def handle_message(event_data):
 # for dict/formatted messages, use send_message(channel, **msg)
 def send_message(channel, **msg):
   slack_client.api_call("chat.postMessage", channel=channel, **msg)
+
+def search(channel):
+  search_results = yelp_api.search("lunch", "pittsburgh, pa", 3)
+
+  restaurants_arr = []
+  reviews_arr = []
+  for res in search_results["businesses"]:
+    restaurant_id = res["id"]
+    restaurants_arr.append(yelp_api.get_business(restaurant_id))
+    reviews_arr.append(yelp_api.get_reviews(restaurant_id))
+
+  send_message(channel, **build_vote_message(restaurants_arr, reviews_arr))
--- format.py
+++ format.py
@@ -6,9 +6,32 @@ footer_icon = "https://www.yelp.com/favicon.ico"
 placeholder_image = "https://s3-media3.fl.yelpcdn.com/assets/srv0/yelp_styleguide/fe8c0c8725d3/assets/img/default_avatars/business_90_square.png"
 maps_api = "https://www.google.com/maps/search/?api=1&"
 
+def build_vote_message(restaurant_arr, reviews_arr):
+  msg = {"text": "I found these options:", "attachments": []}
+  for i in range(len(restaurant_arr)):
+    msg["attachments"].append(format_with_vote_button(
+      restaurant_arr[i], reviews_arr[i]))
+  return msg
+
+# formats the given restaurant and review message
+# uses format_restaurant
+# adds necessary voting bits
+def format_with_vote_button(restaurant, reviews):
+  formatted = format_restaurant(restaurant, reviews)
+  formatted["callback_id"] = "vote"
+  formatted["actions"] = [{
+    "name": "vote",
+    "text": "Vote!"
+    "name": restaurant["name"],
+    "type": "button"
+  }]
+  formatted["fields"].append({"title": "Votes":, "value": 0})
+  return formatted
+
 # takes in the result of 2 Yelp API calls
-# nicely formats a restaurant with relevant info
+# nicely formats a restaurant as an attachment
 # TODO: use the stars that Yelp requires
+# TODO: address/nav-link
 def format_restaurant(restaurant, reviews):
   # we need a name and URL no matter what
   if not ("name" in restaurant and "url" in restaurant):
@@ -16,31 +39,38 @@ def format_restaurant(restaurant, reviews):
   name = restaurant["name"]
   link = restaurant["url"]
 
-  formatted = {}
-  restaurant = {}
+  rating = get_rating(restaurant)
+  snippet = get_review_snippet(reviews)
+  image = get_image(restaurant)
+  price = get_price(restaurant)
 
-  formatted["attachments"] = attachments
+  time = "Currently closed."
+  if is_open(restaurant):
+    time = "Open until {}".format(get_closing_time)
 
-  formatted["text"] = "I found {}! ({})".format(name, get_categories(restaurant))
-  restaurant["fallback"] = name
-  restaurant["color"] = sidebar_color
-  restaurant["author_name"] = get_rating(restaurant)
-  restaurant["author_link"] = link
-  restaurant["title"] = name
-  restaurant["title_link"] = link
-  restaurant["text"] = get_review_snipet(reviews)
-  restaurant["thumb_url"] = get_image(restaurant)
-  restaurant["footer"] = footer
-  restaurant["footer_icon"] = footer_icon
-  restaurant["fields"] = [{"title": "Open today until {} / {}".format(get_time(restaurant), get_price(restaurant))}]
+  restaurant_attachment = {}
+  restaurant_attachment["fallback"] = name
+  restaurant_attachment["color"] = sidebar_color
+  restaurant_attachment["author_name"] = rating
+  restaurant_attachment["author_link"] = link
+  restaurant_attachment["title"] = name
+  restaurant_attachment["title_link"] = link
+  restaurant_attachment["text"] = snippet
+  restaurant_attachment["thumb_url"] = image
+  restaurant_attachment["footer"] = footer
+  restaurant_attachment["footer_icon"] = footer_icon
+  restaurant_attachment["fields"] = [
+    {"title": time, "short": "true"},
+    {"title": price, "short": "true"},
+  ]
 
-  return formatted
+  return restaurant_attachment
 
 # get price with fallback
 def get_price(restaurant):
   if "price" in restaurant:
     return restaurant["price"]
-  return "Unknown Price"
+  return None
 
 # get image with fallback
 def get_image(restaurant):
@@ -55,32 +85,49 @@ def get_rating(restaurant):
     return "\u2605" * int(restaurant["rating"]) + "\u2606" * (5 - int(restaurant["rating"]))
   return "\u2606" * 5
 
+# get current open status
+def is_open(restaurant):
+  if "hours" in restaurant and "is_open_now" in restaurant["hours"]:
+    return restaurant["hours"]["is_open_now"]
+  return False
+
 # get today's closing time with fallback
-def get_time(restaurant):
-  if "time" in restaurant:
+def get_closing_time(restaurant):
+  if "hours" in restaurant:
     weekday = datetime.datetime.today().weekday()
     endtime = restaurant["hours"][0]["open"][weekday]["end"]
     return endtime[:2] + ":" + endtime[2:]
-  return "Unknown Closing Time"
+  return None
 
 # get categories
 def get_categories(restaurant):
   if "categories" in restaurant and len(restaurant["categories"]) > 0:
     return "/".join(map((lambda cat: cat["title"]), restaurant["categories"]))
-  return "Unknown Categories"
+  return None
 
 # get review snippet
-def get_review_snipet(reviews):
-  if "reviews" in reviews and len(reviews["reviews"]) > 0:
-    return reviews["reviews"][0]["text"]
-  return ""
+def get_review_snippet(reviews):
+  current_rating = -1
+  best_review = None
+
+  if "reviews" in reviews:
+    for r in reviews["reviews"]:
+      if "rating" in r and "text" in r:
+        if int(r["rating"]) > current_rating:
+          current_rating = int(r["rating"])
+          best_review = r["text"]
+  return best_review
 
 # get address
 def get_address(restaurant):
   if "location" in restaurant and "display_address" in restaurant["location"]:
     return ", ".join(restaurant["location"]["display_address"])
-  return "Unknown address"
+  return None
 
 # get a link for navigation
-def get_nav_link(address):
-  return maps_api + urllib.parse.urlencode({"query": address})
+# TODO touch this up
+def get_nav_link(restaurant):
+  address = get_address(restaurant)
+  if address:
+    return maps_api + urllib.parse.urlencode({"query": address})
+  return None
