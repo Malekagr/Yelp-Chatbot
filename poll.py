ff4c107e555019a38e926e8a7cf15b307e8fa8fd
Yoshino <weihaomouse@gmail.com>
2018-03-22 21:34:52 -0400

    Update poll.py

--- poll.py
+++ poll.py
@@ -17,10 +17,14 @@ class Poll(object):
                 self._votes[res_name] = 1
     
     def _update_attachments(self):
+        displayed_res_names = []
+        actual_stored_names = []
         if "attachments" in self._slack_msg:
             for att in self._slack_msg["attachments"]:
                 restaurant_name = att["actions"][0]["value"]
+                displayed_res_names.append(restaurant_name)
                 att["fields"][2]["value"] = self._votes.get(restaurant_name) if restaurant_name in self._votes else 0
+        print("displayed_res_names:",displayed_res_names,"\nactual_stored_names:", self._votes.keys())
     
     def get_updated_attachments(self):
         return self._slack_msg
