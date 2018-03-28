0445699f4ac54832c5ef9a14152e081b61478adc
Yoshino <weihaomouse@gmail.com>
2018-03-28 14:34:02 -0400

    Update poll.py

--- poll.py
+++ poll.py
@@ -6,29 +6,29 @@ class Poll(object):
         self._slack_msg = message # the return value of slack_format.build_vote_message
         self._user_votes = user_votes # key=user id, value=button value, a cached value in the flask app
         self._votes = {} # key=restaurant name (the value of the button), value=total votes for the restaurant as integer
-        
+
         self._calculate_votes()
         self._update_attachments()
-    
+
     def _calculate_votes(self):
         for res_name in self._user_votes.values():
             if res_name in self._votes:
                 self._votes[res_name] += 1
             else:
                 self._votes[res_name] = 1
-    
+
     def _update_attachments(self):
         if "attachments" in self._slack_msg:
             for att in self._slack_msg["attachments"]:
                 restaurant_name = cgi.escape(att["actions"][0]["value"])
-                att["fields"][2]["value"] += self._votes.get(restaurant_name) if restaurant_name in self._votes else 0                  
-    
+                att["fields"][2]["title"] = "Votes: {}".format(self._votes.get(restaurant_name) if restaurant_name in self._votes else 0)
+
     def get_updated_attachments(self):
         return self._slack_msg
-    
+
     def get_votes(self):
         return self._votes
-    
+
     @staticmethod
     def get_restaurant_votes(user_votes={}):
         if len(user_votes) == 0:
@@ -40,7 +40,7 @@ class Poll(object):
             else:
                 votes[res_name] = 1
         return votes
-    
+
     @staticmethod
     def get_probabilities(user_votes={}):
         votes = Poll.get_restaurant_votes(user_votes)
@@ -48,7 +48,8 @@ class Poll(object):
         for k, v in votes.items():
             votes[k] = round(v * 1.0 / total, 2)
         return votes
-    
+
+    #return the places with the most votes, and randomly select one of the places if two have tied votes.
     @staticmethod
     def get_winner(user_votes={}):
         restaurant_votes = Poll.get_restaurant_votes(user_votes)
@@ -56,7 +57,7 @@ class Poll(object):
         random.shuffle(winners)
         r = random.randint(0, len(winners)-1)
         return winners[r]
-    
+
     @staticmethod
     def spin_roulette(user_votes={}):
         restaurant_votes = Poll.get_restaurant_votes(user_votes)
@@ -67,12 +68,12 @@ class Poll(object):
         random.shuffle(candidates)
         r = random.randint(0, len(candidates)-1)
         return candidates[r]
-    
-class Finalize(object):    
+
+class Finalize(object):
     @staticmethod
     def conclude(user_votes={}):
         if len(user_votes) == 0:
-            return "No one voted!"
+            return "No one voted!", None
         probs = Poll.get_probabilities(user_votes)
         winner = Poll.get_winner(user_votes)
         s = ""
@@ -80,14 +81,14 @@ class Finalize(object):
             s += "{0} has probability of {1}% to be chosen\n".format(k, v*100)
         return s, winner
 
-    
+
 class ReRoll(object):
     def __init__(self, list_of_ids):
         self._id_list = list_of_ids
-    
+
     def shuffle(self):
         random.shuffle(self._id_list)
-        
+
     def _get_rolls(self):
         self.shuffle()
         new_rolls = []
@@ -100,6 +101,6 @@ class ReRoll(object):
         return new_rolls
     def _get_updated_list(self):
         return self._id_list
-    
+
     def reroll(self):
         return self._get_rolls(), self._get_updated_list()
