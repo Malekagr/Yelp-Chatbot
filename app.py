3a0c1574a6e3b77a28d4fcab0899fae676e60247
Yoshino <weihaomouse@gmail.com>
2018-04-06 10:53:50 -0400

    removed some print statments

--- app.py
+++ app.py
@@ -66,13 +66,9 @@ def message_actions():
     bid_con = AccessBusinessIDs(channel_id, db_conn)
     general_con = AccessGeneral(channel_id, db_conn)
 
-    print("callback_id=", callback_id)
-
     if callback_id == "vote" and float(message_ts) == float(vote_con.get_votes_ts()):
         # this part handles vote buttons
-        #print("Updating votes")
         vote_con.set_user_votes(user_id, selection)
-        #print("updated votes:", vote_con.get_user_votes())
         poll = Poll(vote_con.get_msg_attachments(), vote_con.get_user_votes())
         update_message(channel_id, ts=vote_con.get_votes_ts(), **poll.get_updated_attachments())
 
@@ -87,7 +83,6 @@ def message_actions():
 
     elif callback_id == "invoker_controls":
         if user_id != invoker_con.get_invoker_id():
-            print("Invoker:", invoker_con.get_invoker_id(), "user:", user_id)
             return okay()
 
         if selection == "finalize":
@@ -106,12 +101,10 @@ def message_actions():
         elif selection == "reroll":
           # reroll votes
             business_ids = bid_con.get_business_ids()
-            print('initial b_ids:', business_ids)
             if len(business_ids) < 3 and len(business_ids) > 0:
                 list_of_ids = business_ids
                 bid_con.set_business_ids([])
             elif len(business_ids) <= 0:
-                print("out of ids")
                 slack_client.api_call("chat.postMessage", channel=channel_id, text="There are no more restaurants to reroll.")
                 # don't make any modification to the current poll
                 return okay()
@@ -121,7 +114,6 @@ def message_actions():
                 bid_con.set_business_ids(business_ids)
             # wipes out all votes
             vote_con.reset_user_votes()
-            print('list_of_ids:', list_of_ids)
 
             restaurants_arr = [yelp_api.get_business(restaurant_id) for restaurant_id in list_of_ids]
             msg = slack_format.build_vote_message(restaurants_arr)
@@ -162,7 +154,6 @@ def bot_invoked(event_data):
 
         if not general_con.validate_timestamp(message["ts"]):
             # the received message either has old timestamp or the same value as the current stored one
-            print("Received message too old!")
             return okay()
 
         command_info = parse_command(text)
@@ -175,7 +166,6 @@ def bot_invoked(event_data):
         elif command_info["type"] == "poll":
             # don't proceed if there's an ongoing poll
             if invoked_ts != None and float(invoked_ts) > 0:
-                print("There is an ongoing poll")
                 static_messages.send_busy_message(invoked_channel, slack_client)
                 return okay()
             # renew the terms value if any
